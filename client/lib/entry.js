370f450d6c1382fae598affce65409f27148ee8a
selaias <s.elaias@gmail.com>
2015-12-29 19:33:26 +0000

    initial commit

---
+++ client/lib/entry.js
@@ -0,0 +1,53 @@
+if (typeof AccountsEntry === "undefined") {
+  AccountsEntry = {};
+}
+
+AccountsEntry = {
+  settings: {
+    layoutName: "",
+    contentTemplateName: "",
+    wrapLinks: true,
+    homeRoute: '/home',
+    dashboardRoute: '/dashboard',
+    passwordSignupFields: 'EMAIL_ONLY',
+    emailToLower: true,
+    usernameToLower: false,
+    entrySignUp: '/sign-up',
+    emailVerificationPendingRoute: '/verification-pending',
+    extraSignUpFields: [],
+    showOtherLoginServices: true,
+    requirePasswordConfirmation: true,
+    waitEmailVerification: true
+  },
+  isStringEmail(email) {
+    var emailPattern;
+    emailPattern = /^([\w.-]+)@([\w.-]+)\.([a-zA-Z.]{2,6})$/i;
+    if (email.match(emailPattern)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  config(appConfig) {
+    let signUpRoute;
+    this.settings = _.extend(this.settings, appConfig);
+    i18n.setDefaultLanguage = "en";
+    if (appConfig.language) {
+      i18n.setLanguage = appConfig.language;
+    }
+    if (appConfig.signUpTemplate) {
+      signUpRoute = Router.routes['entrySignUp'];
+      return signUpRoute.options.template || appConfig.signUpTemplate;
+    }
+  },
+  signInRequired(router, extraCondition) {
+    if (extraCondition === null) {
+      extraCondition = true;
+    }
+    if (!Meteor.user()){
+      Session.set('fromWhere', router.url);
+      FlowRouter.go('/sign-in');
+      Session.set('entryError', i18n('error.signInRequired'));
+    }
+  }
+};
---
+++ shared/flow-router.js
@@ -0,0 +1,119 @@
+var exclusions;
+
+var accountsEntryRoutes = FlowRouter.group({
+  prefix: '/accounts',
+  triggersEnter: [trackRouteEntry],
+  triggersExit: [trackRouteEntry]
+});
+
+function trackRouteEntry(context) {
+ 
+}
+
+function trackRouteClose(context) {
+  
+}
+
+Router.route("entrySignIn", {
+  path: "/sign-in",
+  name: 'entrySignIn',
+  template: 'entrySignIn',
+  onBeforeAction: function() {
+    Alerts.clear();
+    Session.set('buttonText', 'in');
+    this.next();
+  },
+  onRun: function() {
+    var pkgRendered, userRendered;
+    if (Meteor.userId()) {
+      Router.go(AccountsEntry.settings.dashboardRoute);
+    }
+    if (AccountsEntry.settings.signInTemplate) {
+      this.template = AccountsEntry.settings.signInTemplate;
+      pkgRendered = Template.entrySignIn.rendered;
+      userRendered = Template[this.template].rendered;
+      if (userRendered) {
+        Template[this.template].rendered = function() {
+          pkgRendered.call(this);
+          return userRendered.call(this);
+        };
+      } else {
+        Template[this.template].rendered = pkgRendered;
+      }
+      Template[this.template].events(AccountsEntry.entrySignInEvents);
+      Template[this.template].helpers(AccountsEntry.entrySignInHelpers);
+    }
+    this.next();
+  }
+});
+Router.route("entrySignUp", {
+  path: "/sign-up",
+  name: 'entrySignUp',
+  template: 'entrySignUp',
+  onBeforeAction: function() {
+    Alerts.clear();
+    Session.set('buttonText', 'up');
+    this.next();
+  },
+  onRun: function() {
+    var pkgRendered, userRendered;
+    if (AccountsEntry.settings.signUpTemplate) {
+      this.template = AccountsEntry.settings.signUpTemplate;
+      pkgRendered = Template.entrySignUp.rendered;
+      userRendered = Template[this.template].rendered;
+      if (userRendered) {
+        Template[this.template].rendered = function() {
+          pkgRendered.call(this);
+          userRendered.call(this);
+        };
+      } else {
+        Template[this.template].rendered = pkgRendered;
+      }
+      Template[this.template].events(AccountsEntry.entrySignUpEvents);
+      Template[this.template].helpers(AccountsEntry.entrySignUpHelpers);
+    }
+    this.next();
+  }
+});
+Router.route("entryForgotPassword", {
+  path: "/forgot-password",
+  name: 'entryForgotPassword',
+  template: 'entryForgotPassword',
+  onBeforeAction: function() {
+    Alerts.clear();
+    this.next();
+  }
+});
+Router.route('entrySignOut', {
+  path: '/sign-out',
+  name: 'entrySignOut',
+  template: 'entrySignOut',
+  onBeforeAction: function() {
+    Alerts.clear();
+    if (AccountsEntry.settings.homeRoute) {
+      Meteor.logout();
+      Router.go(AccountsEntry.settings.homeRoute);
+    }
+    this.next();
+  }
+});
+Router.route('entryResetPassword', {
+  path: 'reset-password/:resetToken',
+  name: 'entryResetPassword',
+  template: 'entryResetPassword',
+  onBeforeAction: function() {
+    Alerts.clear();
+    Session.set('resetToken', this.params.resetToken);
+    this.next();
+  }
+});
+
+Router.route('entryEmailVerificationPending', {
+  path: '/verification-pending',
+  name: 'entryEmailVerificationPending',
+  template: 'entryEmailVerificationPending',
+  onBeforeAction: function() {
+    Alerts.clear();
+    this.next();
+  }
+});
