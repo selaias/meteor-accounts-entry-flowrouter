39b26a1af03fa99b6254ca77f3fd2e96550ec528
selaias <s.elaias@gmail.com>
2016-01-02 20:47:41 +0000

    tests

--- tests/route.js
+++ tests/route.js
@@ -1,5 +1,15 @@
-Router.route('test_helper_home', {
-  path: '/',
+var renderToDiv;
+
+renderToDiv = function(comp) {
+  var div;
+  div = document.createElement("DIV");
+  Blaze.render(comp, div);
+  return div;
+};
+
+FlowRouter.route("/", {
   name: 'test_helper_home',
-  template: 'test_helper_home'
+  action: function() {
+    renderToDiv('test_helper_home');
+  }
 });
